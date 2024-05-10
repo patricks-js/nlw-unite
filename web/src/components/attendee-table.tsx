136cb8832e6998cbe5c6d9d26a228b58512321e3
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-05-09 23:23:32 -0300

    feat: make data with fakerjs and make pagination

--- web/src/components/attendee-table.tsx
+++ web/src/components/attendee-table.tsx
@@ -1,8 +1,15 @@
-import { For } from "solid-js";
+import { attendees } from "@/data/attendees";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { For, createSignal } from "solid-js";
 import { Icons } from "./icons";
 import { Button } from "./ui/button";
 import { Checkbox } from "./ui/checkbox";
 import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "./ui/table";
+
+const [pageIndex, setPageIndex] = createSignal(1);
+
+dayjs.extend(relativeTime);
 
 export function AttendeeTable() {
   return (
@@ -34,21 +41,21 @@ function AttendeeTableHeader() {
 function AttendeeTableBody() {
   return (
     <TableBody>
-      <For each={Array.from({ length: 10 })} fallback={<div>Loading</div>}>
-        {() => (
+      <For each={attendees.slice((pageIndex() - 1) * 10, pageIndex() * 10)} fallback={<div>Loading</div>}>
+        {(item) => (
           <TableRow class="border-b">
             <TableCell>
               <Checkbox id="select-item" />
             </TableCell>
-            <TableCell>12315</TableCell>
+            <TableCell>{item.id}</TableCell>
             <TableCell>
               <div class="flex flex-col gap-1">
-                <span class="font-semibold text-zinc-50">Lucas Patrick</span>
-                <span>lucas@gmail.com</span>
+                <span class="font-semibold text-zinc-50">{item.name}</span>
+                <span>{item.email.toLocaleLowerCase()}</span>
               </div>
             </TableCell>
-            <TableCell>7 days ago</TableCell>
-            <TableCell>7 days ago</TableCell>
+            <TableCell>{dayjs().to(item.createdAt)}</TableCell>
+            <TableCell>{dayjs().to(item.checkedInAt)}</TableCell>
             <TableCell>
               <Button variant={"outline"} size={"icon"}>
                 <Icons.dots size={16} class="text-foreground" />
@@ -62,26 +69,42 @@ function AttendeeTableBody() {
 }
 
 function AttendeeTableFooter() {
+  const maxPages = Math.ceil(attendees.length / 10);
+
+  function goToNextPage() {
+    if (pageIndex() < maxPages) {
+      setPageIndex((prev) => prev + 1);
+    }
+  }
+
+  function goToPrevPage() {
+    if (pageIndex() > 1) {
+      setPageIndex((prev) => prev - 1);
+    }
+  }
+
   return (
     <TableFooter>
       <TableRow>
         <TableCell class="py-3 px-4 text-sm font-semibold" colSpan={3}>
-          Showing 10 of 220 items
+          Showing 10 of {attendees.length} items
         </TableCell>
         <TableCell class="py-3 px-4 text-sm font-semibold text-right" colSpan={3}>
           <div class="inline-flex items-center gap-8">
-            <span>Page 1 of 10</span>
+            <span>
+              Page {pageIndex()} of {maxPages}
+            </span>
             <div class="flex gap-1.5">
-              <Button variant={"secondary"} size={"icon"}>
+              <Button onClick={() => setPageIndex(1)} variant={"secondary"} size={"icon"}>
                 <Icons.cvsLeft size={16} />
               </Button>
-              <Button variant={"secondary"} size={"icon"}>
+              <Button onClick={goToPrevPage} variant={"secondary"} size={"icon"}>
                 <Icons.cvLeft size={16} />
               </Button>
-              <Button variant={"secondary"} size={"icon"}>
+              <Button onClick={goToNextPage} variant={"secondary"} size={"icon"}>
                 <Icons.cvRight size={16} />
               </Button>
-              <Button variant={"secondary"} size={"icon"}>
+              <Button onClick={() => setPageIndex(maxPages)} variant={"secondary"} size={"icon"}>
                 <Icons.cvsRight size={16} />
               </Button>
             </div>
--- web/src/components/search-attendee.tsx
+++ web/src/components/search-attendee.tsx
@@ -1,7 +1,10 @@
+import { createSignal } from "solid-js";
 import { emerald } from "tailwindcss/colors";
 import { Icons } from "./icons";
 
 export function SearchAttendee() {
+  const [, setSearch] = createSignal("");
+
   return (
     <fieldset class="flex items-center gap-3 border border-border px-3 py-1.5 rounded-md text-sm">
       <label for="search" class="sr-only">
@@ -9,6 +12,7 @@ export function SearchAttendee() {
       </label>
       <Icons.search size={16} color={emerald[300]} />
       <input
+        onChange={(e) => setSearch(e.target.value)}
         id="search"
         type="text"
         placeholder="Search for participants..."
